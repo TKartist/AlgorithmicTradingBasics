cba9430b9d340d74df59ff41c70439d39fe65bea
TKartist <rimh@usi.ch>
2023-11-23 23:09:42 +0100

    starting AV

--- udemy/AlgoTrading/alphaVantage.py
+++ udemy/AlgoTrading/alphaVantage.py
@@ -1,3 +1,21 @@
 # Alpha Vantage -> free data collection API, free 5 API calls a minute only
 
 from AV_apiKey import api_key
+
+# returns JSON data
+# Full returns the full history
+# compact returns top 100
+
+from alpha_vantage.timeseries import TimeSeries
+import pandas as pd
+
+ts = TimeSeries(key=api_key, output_format="pandas")
+data = ts.get_daily(symbol="EURUSD", outputsize="full")[0]
+
+data.columns = ["open", "high", "low", "close", "volume"]
+
+all_tickers = ["AAPL", "MSFT", "CSCO", "AMZN", "GOOG", "FB"]
+close_price = pd.DataFrame()
+
+for ticker in all_tickers:
+    starttime = time.time()
