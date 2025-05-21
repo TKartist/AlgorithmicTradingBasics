96a84426b4f9f599a00a7468c4d9d71f23c2d736
TKartist <hunr2001@gmail.com>
2025-05-21 18:03:26 +0200

    adding data retrievers

--- alpaca_app/data_collector.py
+++ alpaca_app/data_collector.py
@@ -1,7 +1,9 @@
-import requests
 import json
 import pandas as pd
-from alpaca.trading.client import TradingClient
+from alpaca.data.historical import StockHistoricalDataClient
+from alpaca.data.requests import StockBarsRequest
+from alpaca.data.timeframe import TimeFrame
+from datetime import datetime
 import os
 from dotenv import load_dotenv
 
@@ -19,11 +21,43 @@ class TradingService:
         api_secret = os.getenv("API_SECRET")
         if not api_key or not api_secret:
             raise ValueError("API_KEY and/or API_SECRET not set in environment.")
-        return TradingClient(api_key, api_secret)
+        return StockHistoricalDataClient(api_key, api_secret)
 
     def get_account_info(self):
         return self.client.get_account()
 
-    def collect_historical_data(self, symbol, start, end, duration):
-        print(f"Collecting historical data for {symbol} from {start} to {end} (duration: {duration})...")
-        '''Implement historical data here lolski'''
+    def collect_historical_data_min(self, symbols, start, end):
+        request_params = StockBarsRequest(
+            symbol_or_symbols=symbols,
+            timeframe=TimeFrame.Minute,
+            start=start,
+            end=end
+        )
+
+        bars = self.client.get_stock_bars(request_params)
+
+        return bars
+
+    def collect_historical_data_day(self, symbols, start, end):
+        request_params = StockBarsRequest(
+            symbol_or_symbols=symbols,
+            timeframe=TimeFrame.Day,
+            start=start,
+            end=end
+        )
+
+        bars = self.client.get_stock_bars(request_params)
+
+        return bars
+    
+    def collect_historical_data_hour(self, symbols, start, end):
+        request_params = StockBarsRequest(
+            symbol_or_symbols=symbols,
+            timeframe=TimeFrame.Hour,
+            start=start,
+            end=end
+        )
+
+        bars = self.client.get_stock_bars(request_params)
+
+        return bars
