d158398407715e3ac3b6a3fd2fc48b9577904a48
TKartist <hunr2001@gmail.com>
2025-06-02 18:59:21 +0200

    thinking of an objective way to describe the candles

--- binance_app/bucket.py
+++ binance_app/bucket.py
@@ -3,31 +3,23 @@ from scipy.stats import gaussian_kde
 import numpy as np
 import matplotlib.pyplot as plt
 
-df = pd.read_csv("bar_info.csv", index_col="open_time")
-df["close_ratio"] = df["close"].pct_change()
-df = df.fillna(0)
+df = pd.read_csv("bar_info.csv")
 
-data = df["close_ratio"].to_numpy()
-bins = np.linspace(-0.2, 0.2, 30)  # 21 edges = 20 bins
+def transform_data(df):
+    print("GOOD")
+    body = df["open"] - df["close"]
+    upper_thres = df[["open", "close"]].max(axis=1)
+    lower_thres = df[["open", "close"]].min(axis=1)
+    increase = df["open"] < df["close"]
+    head = df["high"] - upper_thres
+    tail = lower_thres - df["low"]
 
-# Get counts per bin (density=False = raw counts)
-counts, bin_edges = np.histogram(data, bins=bins, density=False)
+    data = {
+        "body" : body,
+        "head" : head,
+        "tail" : tail,
+        "profit" : increase,
+    }
+    print("GOT HERE")
 
-# Bin centers for plotting (optional)
-bin_centers = 0.5 * (bin_edges[:-1] + bin_edges[1:])
-
-# Put into a DataFrame
-hist_df = pd.DataFrame({
-    "bin_start": bin_edges[:-1],
-    "bin_end": bin_edges[1:],
-    "count": counts
-})
-
-hist_df.to_csv("density_df.csv", index=False)
-
-plt.bar(bin_centers, np.log1p(counts), width=0.02, edgecolor='black', alpha=0.7)
-plt.xlabel("Value Range")
-plt.ylabel("Count")
-plt.title("Histogram Count in [-0.2, 0.2]")
-plt.grid(True)
-plt.savefig("price_change_density.png", dpi=500)
+transform_data(df)
