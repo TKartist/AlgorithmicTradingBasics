6859016884241e49c657fea79d7b12c77a515275
TKartist <hunr2001@gmail.com>
2025-06-02 22:05:32 +0200

    made some changes to interpretation

--- binance_app/bucket.py
+++ binance_app/bucket.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 
 df = pd.read_csv("bar_info.csv", index_col="open_time")
 
+
 '''
 realistically, trading during the asian exchange active hours will be beneficial due to less involvement from
 institutional traders and more abundance of retail traders with aggressive sentiment based strategies.
@@ -13,23 +14,21 @@ It should have more readable and predictable price and volume movements
 
 def transform_data(df):
     print("GOOD")
-    body = df["open"] - df["close"]
     upper_thres = df[["open", "close"]].max(axis=1)
     lower_thres = df[["open", "close"]].min(axis=1)
     true_range = df["close"] - df["open"]
-    tail = (lower_thres - df["low"]) / true_range.abs()
-    head = df["high"] - upper_thres / true_range.abs()
-    true_range = true_range / df["open"]
+    tail = ((lower_thres - df["low"]) / df["low"]) * 100
+    head = ((df["high"] - upper_thres) / df["low"]) * 100
+    true_range_pct = (true_range / df["open"]) * 100
 
     data = {
-        "body" : body,
+        "body" : true_range_pct,
         "head" : head,
         "tail" : tail,
-        "profit" : true_range,
     }
     print("GOT HERE")
     return data
 
-x = transform_data(df[43200:])
+x = transform_data(df)
 df_t = pd.DataFrame(x)
 df_t.to_csv("bar_info_transformed.csv")
